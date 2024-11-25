4a31ded9092658954f50f41b4e9be121257d13f4
stu-k-ry <2310538h@stu.kobe-u.ac.jp>
2024-11-25 22:59:45 +0900

    final work ICON

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,6 +1,7 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let increment = 1;
 
 function setup(){
   createCanvas(200, 200);
@@ -10,7 +11,9 @@ function setup(){
 
 function draw(){
   background(160, 192, 255);
-  count = (count + 1) % cycle;
-  // BLANK[1]
+  count = (count + increment) % cycle;
+  if(keyIsPressed){increment = 2;} 
+  else{increment = 1;}
+  if(count < cycle/2){size = count + 50;} else {size = (cycle - count) + 50;}// BLANK[1]
   ellipse(width / 2, height / 2, size);
 }
--- section4-2/sketch.js
+++ section4-2/sketch.js
@@ -16,14 +16,15 @@ function draw(){
     ellipse(b.x, b.y, b.size);
     b.x += b.vx;
     b.y += b.vy;
+    }
   }
-}
 
 function mouseDragged(){
   const dx = mouseX - pmouseX;
   const dy = mouseY - pmouseY;
   if(mag(dx, dy) > 5){
-    const b = { x: mouseX, y: mouseY, size: 20, vx: dx, vy: dy };
+    const size = random(10, 50);
+    const b = { x: mouseX, y: mouseY, size, vx: dx, vy: dy };
     balls.push(b);
   }
 }
